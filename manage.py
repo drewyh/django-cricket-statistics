476d1da355db10790268d3581aacde10107a8533
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-08-28 09:27:15 +0200

    Switch to pytest ecosystem

--- manage.py
+++ manage.py
@@ -11,29 +11,15 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.test_settings")
     try:
         from django.core.management import execute_from_command_line
-        from django.conf import settings
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    # configure the settings as a standalone app
-    settings.configure(
-        DEBUG=True,
-        DATABASES={
-            "default": {
-                "ENGINE": "django.db.backends.sqlite3",
-                "NAME": str(BASE_DIR / "db.sqlite3"),
-            }
-        },
-        SECRET_KEY="placeholder-key",
-        INSTALLED_APPS=["django_cricket_statistics", "tests"],
-        PASSWORD_HASHERS=["django.contrib.auth.hashers.MD5PasswordHasher"],
-    )
 
     execute_from_command_line(sys.argv)
 
