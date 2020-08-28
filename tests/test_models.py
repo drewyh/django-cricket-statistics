c20ce50a3596c42c5c5e052f9f964adf21421b00
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-08-28 09:53:11 +0200

    Use pytest style tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,29 +1,28 @@
 """Test the views for cricket statistics."""
 
-from django.test import TestCase
+import pytest
 
 from django_cricket_statistics.views import BattingAverageSeasonView
 from django_cricket_statistics.models import Player
 
 
-class PlayerTestCase(TestCase):
-    def setUp(self):
-        Player.objects.create(
-            first_name="John",
-            nickname="Jack",
-            middle_names="George Henry",
-            last_name="Smith",
-        )
+@pytest.fixture
+def player(db):
+    return Player.objects.create(
+        first_name="John",
+        nickname="Jack",
+        middle_names="George Henry",
+        last_name="Smith",
+    )
 
-    def test_player_string(self):
-        player = Player.objects.get(first_name="John")
-        self.assertEqual(str(player), "JGH Smith")
 
-    def test_player_long_name(self):
-        player = Player.objects.get(first_name="John")
-        self.assertEqual(player.long_name, "John George Henry (Jack) Smith")
+def test_player_string(db, player):
+    assert str(player) == "JGH Smith"
 
-    def test_player_short_name(self):
-        player = Player.objects.get(first_name="John")
-        self.assertEqual(player.short_name, "JGH Smith")
 
+def test_player_long_name(db, player):
+    assert player.long_name == "John George Henry (Jack) Smith"
+
+
+def test_player_short_name(db, player):
+    assert player.short_name == "JGH Smith"
