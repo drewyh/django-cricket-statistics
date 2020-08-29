2bf1fb0ed0b5ddf5a4ac8159c78861c381e0cb2e
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-08-29 11:11:09 +0200

    Split views into multiple files

---
+++ django_cricket_statistics/views/__init__.py
@@ -0,0 +1,5 @@
+"""Views for cricket statistics."""
+
+from django_cricket_statistics.views.common import *
+from django_cricket_statistics.views.batting import *
+from django_cricket_statistics.views.bowling import *
