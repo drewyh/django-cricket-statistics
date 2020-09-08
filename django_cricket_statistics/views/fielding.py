58344b1b3b0944c822b0158a6335a0789bc06205
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-09-08 15:18:47 +0200

    Add fielding and wk views

--- django_cricket_statistics/views/fielding.py
+++ django_cricket_statistics/views/fielding.py
@@ -1,6 +1,40 @@
 """Views for fielding statistics."""
 
-# from django_cricket_statistics.views.common import CareerStatistic, SeasonStatistic
+from django_cricket_statistics.views.common import CareerStatistic, SeasonStatistic
+from django_cricket_statistics.statistics import FIELDING_CATCHES, FIELDING_RUN_OUTS
 
-# class FieldingCatchesCareerView(CareerStatistic):
-# class FieldingCatchesSeasonView(SeasonStatistic):
+
+class FieldingCatchesCareerView(CareerStatistic):
+    """Most career fielding catches."""
+
+    aggregates = FIELDING_CATCHES
+    ordering = "-fielding_catches__sum"
+    filters = {"fielding_catches__sum__gt": 0}
+    columns_extra = {"fielding_catches__sum": "Ct"}
+
+
+class FieldingCatchesSeasonView(SeasonStatistic):
+    """Most season fielding catches."""
+
+    aggregates = FIELDING_CATCHES
+    ordering = "-fielding_catches__sum"
+    filters = {"fielding_catches__sum__gt": 0}
+    columns_extra = {"fielding_catches__sum": "Ct"}
+
+
+class FieldingRunOutsCareerView(CareerStatistic):
+    """Most career fielding runouts (assisted and unassisted)."""
+
+    aggregates = FIELDING_RUN_OUTS
+    ordering = "-fielding_run_outs__sum"
+    filters = {"fielding_run_outs__sum__gt": 0}
+    columns_extra = {"fielding_run_outs__sum": "RO"}
+
+
+class FieldingRunOutsSeasonView(SeasonStatistic):
+    """Most season fielding runouts (assisted and unassisted)."""
+
+    aggregates = FIELDING_RUN_OUTS
+    ordering = "-fielding_run_outs__sum"
+    filters = {"fielding_run_outs__sum__gt": 0}
+    columns_extra = {"fielding_run_outs__sum": "RO"}
--- django_cricket_statistics/views/wicketkeeping.py
+++ django_cricket_statistics/views/wicketkeeping.py
@@ -1,10 +1,62 @@
 """Views for wicketkeeping statistics."""
 
-# from django_cricket_statistics.views.common import CareerStatistic, SeasonStatistic
+from django_cricket_statistics.views.common import CareerStatistic, SeasonStatistic
+from django_cricket_statistics.statistics import (
+    WICKETKEEPING_CATCHES,
+    WICKETKEEPING_STUMPINGS,
+    WICKETKEEPING_DISMISSALS,
+)
 
-# class WicketkeepingDismissalsCareerView(CareerStatistic):
-# class WicketkeepingDismissalsSeasonView(SeasonStatistic):
-# class WicketkeepingCatchesCareerView(CareerStatistic):
-# class WicketkeepingCatchesSeasonView(SeasonStatistic):
-# class WicketkeepingStumpingsCareerView(CareerStatistic):
-# class WicketkeepingStumpingsSeasonView(SeasonStatistic):
+
+class WicketkeepingDismissalsCareerView(CareerStatistic):
+    """Most career wicketkeeping dismissals."""
+
+    aggregates = WICKETKEEPING_DISMISSALS
+    ordering = "-wicketkeeping_dismissals__sum"
+    filters = {"wicketkeeping_dismissals__sum__gt": 0}
+    columns_extra = {"wicketkeeping_dismissals__sum": "Dis"}
+
+
+class WicketkeepingDismissalsSeasonView(SeasonStatistic):
+    """Most season wicketkeeping dismissals."""
+
+    aggregates = WICKETKEEPING_DISMISSALS
+    ordering = "-wicketkeeping_dismissals__sum"
+    filters = {"wicketkeeping_dismissals__sum__gt": 0}
+    columns_extra = {"wicketkeeping_dismissals__sum": "WK Dis"}
+
+
+class WicketkeepingCatchesCareerView(CareerStatistic):
+    """Most career wicketkeeping catches."""
+
+    aggregates = WICKETKEEPING_CATCHES
+    ordering = "-wicketkeeping_catches__sum"
+    filters = {"wicketkeeping_catches__sum__gt": 0}
+    columns_extra = {"wicketkeeping_catches__sum": "WK Ct"}
+
+
+class WicketkeepingCatchesSeasonView(SeasonStatistic):
+    """Most season wicketkeeping catches."""
+
+    aggregates = WICKETKEEPING_CATCHES
+    ordering = "-wicketkeeping_catches__sum"
+    filters = {"wicketkeeping_catches__sum__gt": 0}
+    columns_extra = {"wicketkeeping_catches__sum": "WK Ct"}
+
+
+class WicketkeepingStumpingsCareerView(CareerStatistic):
+    """Most career wicketkeeping stumpings."""
+
+    aggregates = WICKETKEEPING_STUMPINGS
+    ordering = "-wicketkeeping_stumpings__sum"
+    filters = {"wicketkeeping_stumpings__sum__gt": 0}
+    columns_extra = {"wicketkeeping_stumpings__sum": "WK St"}
+
+
+class WicketkeepingStumpingsSeasonView(SeasonStatistic):
+    """Most season wicketkeeping stumpings."""
+
+    aggregates = WICKETKEEPING_STUMPINGS
+    ordering = "-wicketkeeping_stumpings__sum"
+    filters = {"wicketkeeping_stumpings__sum__gt": 0}
+    columns_extra = {"wicketkeeping_stumpings__sum": "WK St"}
