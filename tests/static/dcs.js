5360b7060f68a47646b472cbd2f9aa27a9eb92e1
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-09-10 16:02:31 +0200

    Further template cleanup

--- tests/static/dcs.js
+++ tests/static/dcs.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $("table").addClass("table table-striped table-bordered table-hover table-sm table-responsive");
-  $("ul.pages").addClass("pagination pagination-sm justify-content-center");
-  $("ul.pages li").addClass("page-item");
-  $("ul.pages li a").addClass("page-link");
-  $("ul.pages li span").addClass("page-link");
-  $("ul.pages li span").parent().addClass("disabled");
+  $("nav.paginator ul").addClass("pagination pagination-sm justify-content-center");
+  $("nav.paginator ul li").addClass("page-item");
+  $("nav.paginator ul li a").addClass("page-link");
+  $("nav.paginator ul li span").addClass("page-link");
+  $("nav.paginator ul li span").parent().addClass("disabled");
   $("div.link-list a").addClass("btn btn-primary btn-block");
 })
