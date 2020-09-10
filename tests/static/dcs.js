10c7f490c72c10602e7f39d1a72fa8009948a661
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-09-10 11:09:12 +0200

    Cleanup templates

--- tests/static/dcs.js
+++ tests/static/dcs.js
@@ -1,1 +1,6 @@
-$("table").addClass("table table-striped table-bordered table-hover table-sm table-responsive")
+$(document).ready(function(){
+  $("table").addClass("table table-striped table-bordered table-hover table-sm table-responsive");
+  $("ul.pages").addClass("pagination pagination-sm justify-content-center");
+  $("ul.pages li").addClass("page-item");
+  $("ul.pages li a").addClass("page-link");
+})
