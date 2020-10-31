2ad924ea2496f2064938099ac5b7eb95e8341622
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-10-31 15:09:33 +0000

    Cleanup project specific urls

--- django_cricket_statistics/urls.py
+++ django_cricket_statistics/urls.py
@@ -6,7 +6,6 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.views.generic.base import View
 
-from tests import test_settings as settings
 from django_cricket_statistics import views
 
 HOMEPAGE_PATTERNS = {
@@ -151,30 +150,4 @@ urlpatterns = [
         ),
         name="index",
     ),
-    path("admin/", admin.site.urls),
-    path(
-        "admin/password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="admin_password_reset",
-    ),
-    path(
-        "admin/password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
